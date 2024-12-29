24f091c477cf68467db391a3c593c9eb9bc76f13
Kien Dinh <kiendinh@Kiens-Mac-mini.local>
2024-12-29 16:58:49 -0700

    mess

---
+++ src/tutorial.ts
@@ -0,0 +1,19 @@
+let greeting: string = 'Hello, Typescript'
+greeting = greeting.toUpperCase()
+console.log(greeting)
+
+let age: number = 25
+age = age + 5
+console.log(age)
+
+let isAdult: boolean = age >= 10
+
+isAdult = !isAdult
+
+console.log(isAdult)
+
+// greeting = 10
+// age = 'thirty'
+// isAdult = 'yes'
+
+console.log(greeting, age, isAdult)
